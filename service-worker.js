6192e62a5aed65ee90c0664b63cd35ad1d7fa20b
vanessasawanvs-svg <vanessa.sawan.vs@gmail.com>
2026-09-22 22:19:29 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='core-theory-v9-11-mobile-drawer';
+const CACHE='core-theory-v9-12-text-wordmark';
 const PRECACHE=['/','/index.html','/styles.css','/app.js','/manifest.json','/offline.html','/icons/icon-192.png','/icons/icon-512.png','/icons/icon-maskable-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE)))});
 self.addEventListener('activate',e=>e.waitUntil((async()=>{const ks=await caches.keys();await Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim()})()));
